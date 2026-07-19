65357d0d4529ed46d3800a3bdb1ecb498b070209
Ayush Pal <nitianayush8712@gmail.com>
2026-07-19 10:57:48 +0530

    setup project files part 1

--- models/ecommerce/category.model.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-const categoryschema = mongoose.Schema({
-    name:{
-        type:String,
-        required:true,
-    }
-
-},{timestamps:true})
-export const Category = mongoose.model("Category",categoryschema)
